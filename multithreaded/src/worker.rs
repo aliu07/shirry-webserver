3afc993ce16200d3b9a9accdef554c9eb3a885a6
Alexander Liu <alexander_l_22@hotmail.com>
2025-05-27 13:36:02 -0400

    Add unit tests for worker module

--- multithreaded/src/worker.rs
+++ multithreaded/src/worker.rs
@@ -43,3 +43,56 @@ impl Worker {
         self.thread.take().unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_worker_id() {
+        let (_, rx) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(rx));
+        let worker = Worker::new(5, Arc::clone(&receiver));
+
+        assert_eq!(worker.get_id(), 5);
+    }
+
+    #[test]
+    fn take_thread_from_worker() {
+        let (_, rx) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(rx));
+        let mut worker = Worker::new(5, Arc::clone(&receiver));
+
+        worker.take_thread();
+
+        assert!(worker.thread.is_none());
+    }
+
+    #[test]
+    fn worker_executes_job() {
+        let (tx, rx) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(rx));
+        let mut worker = Worker::new(5, Arc::clone(&receiver));
+
+        let result = Arc::new(Mutex::new(0));
+
+        {
+            let result = Arc::clone(&result);
+            let job = Box::new(move || {
+                let mut res = result.lock().unwrap();
+                *res += 5;
+            });
+
+            tx.send(job).unwrap();
+        }
+
+        // Disconnect worker
+        drop(tx);
+
+        // Wait for worker to finish job
+        worker.take_thread().join().unwrap();
+
+        let res = result.lock().unwrap();
+        assert_eq!(*res, 5);
+    }
+}
